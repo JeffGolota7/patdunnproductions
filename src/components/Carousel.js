0bfd96b04ce3d55327845cdf2a8646ae1cd6798c
jeffgolota <jeff.g@iqm.com>
2023-08-31 11:05:37 -0400

    minor fixes

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -4,8 +4,8 @@ import "../styles/carousel.css";
 
 export default function Carousel() {
   const [videos, setVideos] = useState([]);
-  const [isDragStart, setDragStart] = useState(false);
   const [videoToDisplay, updateVideoToDisplay] = useState();
+  const [isClick, updateIsClick] = useState();
   const importAll = (r) => r.keys().map(r);
   const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent);
 
@@ -28,11 +28,17 @@ export default function Carousel() {
   useEffect(() => {
     const carousel = document.querySelector(".videos-container");
 
-    setDragStart(false);
+    let isDragStart = false;
+    updateIsClick(false);
     let prevPageX, prevScrollLeft;
+    let clickX = 0;
+    let clickY = 0;
 
     const dragStart = (e) => {
-      setDragStart(true);
+      isDragStart = true;
+      updateIsClick(true);
+      clickX = e.pageX;
+      clickY = e.pageY;
       prevPageX = e.pageX;
       prevScrollLeft = carousel.scrollLeft;
     };
@@ -40,24 +46,50 @@ export default function Carousel() {
     const dragging = (e) => {
       if (!isDragStart) return;
       e.preventDefault();
-      let positionDiff = e.pageX - prevPageX;
+      const xDiff = Math.abs(e.pageX - clickX);
+      const yDiff = Math.abs(e.pageY - clickY);
+      if (xDiff > 10 || yDiff > 10) {
+        updateIsClick(false);
+      } else {
+        updateIsClick(true);
+      }
+
+      const touchX = e.touches[0].clientX;
+      console.log(touchX);
+      let positionDiff = (touchX || e.pageX) - prevPageX;
+      carousel.scrollLeft = prevScrollLeft - positionDiff;
+    };
+
+    const draggingMobile = (e) => {
+      if (!isDragStart) return;
+      e.preventDefault();
+      const xDiff = Math.abs(e.pageX - clickX);
+      const yDiff = Math.abs(e.pageY - clickY);
+      if (xDiff > 10 || yDiff > 10) {
+        updateIsClick(false);
+      } else {
+        updateIsClick(true);
+      }
+
+      const touchX = e.touches[0].clientX;
+      let positionDiff = touchX - prevPageX;
       carousel.scrollLeft = prevScrollLeft - positionDiff;
     };
 
     const dragStop = () => {
-      setDragStart(false);
+      isDragStart = false;
     };
 
     carousel.addEventListener("mousedown", dragStart);
-    carousel.addEventListener("touchstart", dragStart);
+    carousel.addEventListener("touchstart", dragStart, { passive: false });
 
     carousel.addEventListener("mousemove", dragging);
-    carousel.addEventListener("touchmove", dragging);
+    carousel.addEventListener("touchmove", draggingMobile);
 
     carousel.addEventListener("mouseup", dragStop);
     carousel.addEventListener("mouseleave", dragStop);
     carousel.addEventListener("touchend", dragStop);
-  }, [isDragStart]);
+  });
   return (
     <div className="video-carousel">
       {videoToDisplay && (
@@ -69,11 +101,11 @@ export default function Carousel() {
       )}
       <div className="videos-container">
         {videos.map((vid) => (
-          <>
+          <div className="video">
             <video
               src={vid}
-              className="video"
               type="video/mp4"
+              className="actual-video"
               muted
               playsInline
               onMouseOver={(event) => {
@@ -85,12 +117,41 @@ export default function Carousel() {
                 event.target.pause();
               }}
               onClick={() => {
-                if (!isDragStart) {
+                console.log(isClick);
+                if (isClick) {
                   updateVideoToDisplay(vid);
                 }
               }}
             ></video>
-          </>
+            <div className="thumbnail-overlay">
+              <svg
+                fill="#000000"
+                height="800px"
+                width="800px"
+                version="1.1"
+                id="Capa_1"
+                viewBox="0 0 481 481"
+                className="playIcon"
+              >
+                <g>
+                  <g>
+                    <path
+                      fill="#ffffff"
+                      d="M410.6,70.4C365.1,25,304.7,0,240.5,0S115.9,25,70.4,70.4C25,115.9,0,176.3,0,240.5s25,124.6,70.4,170.1
+			C115.8,456,176.2,481,240.5,481s124.6-25,170.1-70.4C456,365.2,481,304.8,481,240.5S456,115.9,410.6,70.4z M240.5,454
+			C122.8,454,27,358.2,27,240.5S122.8,27,240.5,27S454,122.8,454,240.5S358.2,454,240.5,454z"
+                    />
+                    <path
+                      fill="#ffffff"
+                      d="M349.2,229.1l-152.6-97.9c-4.2-2.7-9.4-2.9-13.8-0.5c-4.3,2.4-7,6.9-7,11.8v195.7c0,4.9,2.7,9.5,7,11.8
+			c2,1.1,4.3,1.7,6.5,1.7c2.5,0,5.1-0.7,7.3-2.1l152.6-97.9c3.9-2.5,6.2-6.8,6.2-11.4S353,231.6,349.2,229.1z M202.8,313.7V167.3
+			l114.1,73.2L202.8,313.7z"
+                    />
+                  </g>
+                </g>
+              </svg>
+            </div>
+          </div>
         ))}
       </div>
     </div>
